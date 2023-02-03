f4cbcb8b55b99905d5de2e2060e2f2ab70284022
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-02-02 21:09:15 -0300

    front-end e Back-end da aplicacao

---
+++ README.md
@@ -0,0 +1,14 @@
+# Buscar-Letras
+Aplicação Web Para Buscar Letra De Musicas Atraves de um serviço de api...
+
+Hospedagem:
+
+servidor (1): https://buscarletra.netlify.app
+
+servidor (2): https://buscarletra.vercel.app
+
+Apis Usadas:
+
+api (1): https://api.lyrics.ovh/
+
+api (2): https://api.vagalume.com.br/search.php
---
+++ script/app.js
@@ -0,0 +1,81 @@
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search');
+const songsContainer = document.querySelector('#songs-container');
+const prevAndNextContainer = document.querySelector("#prev-and-next-container");
+
+const apiUrl = 'https://api.lyrics.ovh';
+
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await response.json()
+    insertSongsIntoPage(data)
+}
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = (songsInfo.data.map(song => `
+    <li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver Letra</button>
+    </li>
+    `).join(''))
+
+
+    if(songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : '' }
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : '' }
+        `
+        return
+    }
+
+    prevAndNextContainer.innerHTML = ''
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiUrl}/suggest/${term}`)
+    const data = await response.json()
+
+    insertSongsIntoPage(data)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const searchTerm = searchInput.value.trim()
+
+    if(!searchTerm) {
+        songsContainer.innerHTML = `<li class="warning-message">Por Favor, Digite Um Termo Válido.</li>`
+        return
+    }
+
+    fetchSongs(searchTerm)
+});
+
+const apiVagalume = 'https://api.vagalume.com.br/search.php?'
+const apiKey = '28cb27e6d6a282e7ae827b052e4ab2b4'
+
+const fetchLyrics = async (artist, songTitle) => {
+    const response = await fetch(`${apiVagalume}art=${artist}&mus=${songTitle}&apikey=${apiKey}`)
+    const data = await response.json()
+    const lyric = data.mus[0].text.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    songsContainer.innerHTML = `
+        <li class="lyrics-container">
+            <h2><strong>${songTitle}</strong> - ${artist}</h2>
+            <p class="lyrics">${lyric}</p>
+        </li>
+    `
+}
+
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+
+    if(clickedElement.tagName === 'BUTTON'){
+       const artist = clickedElement.getAttribute('data-artist')
+       const songTitle = clickedElement.getAttribute('data-song-title')
+
+
+       prevAndNextContainer.innerHTML = ''
+       fetchLyrics(artist, songTitle)
+    }
+})
